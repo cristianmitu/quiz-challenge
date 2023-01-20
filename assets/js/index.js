93ece964ccc974f63549441c0d16ee58c7ae27c1
Cristian Mitu <mitu_christian@yahoo.com>
2023-01-20 14:07:11 +0000

    modified the for loop for the answers options

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
 let questions = [
-    {question: "1" ,answers :["ex1-1" , "ex1-2" , "ex1-3"], corectAnswer : "1"},
-    {question: "2" ,answers :["ex2-1" , "ex2-2" , "ex2-3"], corectAnswer : "2"},
-    {question: "3" ,answers :["ex3-1" , "ex3-2" , "ex3-3"], corectAnswer : "3"},
-    {question: "4" ,answers :["ex4-1" , "ex4-2" , "ex4-3"], corectAnswer : "4"},
-    {question: "5" ,answers :["ex5-1" , "ex5-2" , "ex5-3"], corectAnswer : "5"},
-    {question: "6" ,answers :["ex6-1" , "ex6-2" , "ex6-3"], corectAnswer : "6"},
-    {question: "7" ,answers :["ex7-1" , "ex7-2" , "ex7-3"], corectAnswer : "7"},
+    {question: "1" ,answers :["ex1-1" , "ex1-2" , "ex1-3"], correctAnswer : "1"},
+    {question: "2" ,answers :["ex2-1" , "ex2-2" , "ex2-3"], correctAnswer : "2"},
+    {question: "3" ,answers :["ex3-1" , "ex3-2" , "ex3-3"], correctAnswer : "3"},
+    {question: "4" ,answers :["ex4-1" , "ex4-2" , "ex4-3"], correctAnswer : "4"},
+    {question: "5" ,answers :["ex5-1" , "ex5-2" , "ex5-3"], correctAnswer : "5"},
+    {question: "6" ,answers :["ex6-1" , "ex6-2" , "ex6-3"], correctAnswer : "6"},
+    {question: "7" ,answers :["ex7-1" , "ex7-2" , "ex7-3"], correctAnswer : "7"},
 ];
 
 let startQuizEl =  document.querySelector("#start");
@@ -16,12 +16,15 @@ let questionsTitleEl = document.querySelector("#question-title");
 let choicesEl = document.querySelector("#choices");
 let timerEl = document.querySelector("#time");
 let finalScore = document.querySelector("#final-score");
+let initials = document.querySelector("#initials");
+let submitInitials = document.querySelector("#submit");
 let quizTimer = 100;
 let quizInterval;
 let currentQuestion;
 let answers;
+let answersOptions;
 let userScore;
-let corectAnswer;
+let correctAnswer;
 let failSound = new Audio("./assets/sfx/incorrect.wav")
 let successSound = new Audio("./assets/sfx/correct.wav")
 
@@ -30,10 +33,12 @@ function startQuiz(){
 startQuizEl.addEventListener("click",function(event){
     startScreenEl.setAttribute("class" , "hide");
     questionsEl.setAttribute("class","start");
+    choicesEl.setAttribute("class","start");
     quizInterval = setInterval(function(){    
         quizTimer--;    
         timerEl.textContent = quizTimer;
-        },1000);              
+        },1000); 
+                     
 });
 }
 //end quiz
@@ -49,27 +54,34 @@ function endQuiz() {
 }
 startQuiz();
 
-function quizQuestions() {
+
+
+function nextQuestion(){
     
     for (let i = 0; i < questions.length; i++) {  
-        
+        currentQuestion = "";
         currentQuestion = questions[i].question;    
-        corectAnswer = questions[i].corectAnswer[0];
+        correctAnswer = questions[i].correctAnswer[0];
         questionsTitleEl.textContent = currentQuestion;
         //questionsEl.setAttribute("class",""); 
         console.log("current question",currentQuestion);
-        console.log("corect answer : ",corectAnswer);
-        for (let j = 0; j < questions[i].answers.length; j++) {
-            answers = questions[i].answers[j];
-            let answersBtn = document.createElement("button");            
-            answersBtn.textContent = answers;
-            choicesEl.appendChild(answersBtn);
-            console.log("answers : ",answers);
-        }
-        
+        console.log("corect answer : ",correctAnswer);
+        answers = questions[i].answers; 
+        console.log("answers : ",answers);            
         console.log(questionsTitleEl.textContent = currentQuestion);
     };
-    return currentQuestion , answers;
-}
-quizQuestions();
     
+    for (let i = 0; i < answers.length; i++) {
+        answersOptions = [];
+        answersOptions = answers[i];
+        let answerBtn = document.createElement("button");
+        answerBtn.textContent = answersOptions;
+        choicesEl.appendChild(answerBtn);
+        console.log("answers : ",answersOptions);
+        
+    }
+    return currentQuestion , answersOptions;
+
+    
+}    
+nextQuestion();
